b0f277923b659217f96b2aee4435fd1358e0792d
steve <siannopollo@macBook.local>
2008-03-01 14:29:00 -0500

    Printlog can now sort by date

--- _printlog/printlog.rb
+++ _printlog/printlog.rb
@@ -1,39 +1,102 @@
 require "date"
 class Printlog
-  attr_reader :directory, :limit, :developer
-  
-  def initialize(directory, limit=15, developer=nil)
-    @directory, @limit, @developer = directory, limit, developer
+  attr_reader :directory, :limit, :developer, :formatter, :report, :dates
+  def initialize(directory, options={})
+    @directory = directory.gsub("\n", "")
+    parse_options(options)
+    @formatter = eval("#{scm.upcase}Formatter.new(self)")
   end
   
-  def log
-    @log ||= `svn log #{directory.gsub("\n", "")} --limit #{limit}`
+  def parse_options(options)
+    defaults.update(options).each {|k, v| instance_variable_set(:"@#{k}", v)}
   end
   
-  def separator
-    @separator ||= log.scan(/^-.*?-$/).first
+  def defaults
+    {:limit => 15, :developer => nil, :dates => nil, :format => "plain"}
   end
   
-  def log_entries
-    @log_entries ||= log.split(separator)[1..-2].collect {|entry| LogEntry.new(entry, self)}
+  def scm
+    @scm ||= `test -d #{directory}/.git && echo git || echo svn`.gsub("\n", "")
   end
   
   def report
-    log_entries.collect do |entry|
-      next unless entry.developer == developer || developer.nil?
-      entry.report
-    end.compact
+    formatter.report
+  end
+  
+  def real_dates
+    dates.nil? ? [] : dates.split("-").collect {|d| Date.parse(d)}
+  end
+  
+  def start_date
+    real_dates.empty? ? nil :
+      (real_dates.first < real_dates.last ? real_dates.first : real_dates.last)
+  end
+  
+  def end_date
+    (real_dates - [start_date]).first
+  end
+  
+  class Formatter
+    attr_reader :printer
+    def initialize(printer)
+      @printer = printer
+    end
+    
+    def formatted_log_entries
+      if printer.dates.nil?
+        log_entries
+      else
+        log_entries.select {|entry| printer.start_date <= entry.date && entry.date <= printer.end_date}
+      end
+    end
+  end
+  
+  class SVNFormatter < Formatter
+    def log
+      @log ||= `svn log #{printer.directory} --limit #{printer.limit}`
+    end
+    
+    def separator
+      @separator ||= log.scan(/^-.*?-$/).first
+    end
+    
+    def log_entries
+      @log_entries ||= log.split(separator)[1..-2].collect {|entry| SVNLogEntry.new(entry, printer)}
+    end
+    
+    def report
+      formatted_log_entries.collect do |entry|
+        next unless entry.developer == printer.developer || printer.developer.nil?
+        entry.report
+      end.compact
+    end
+  end
+  
+  class GITFormatter < Formatter
+    def log
+      @log ||= `cd #{printer.directory} && git log -n#{printer.limit}`
+    end
+    
+    def separator
+      @separator ||= "commit "
+    end
+    
+    def log_entries
+      @log_entries ||= log.split(separator)[1..-1].collect {|entry| GITLogEntry.new(entry, printer)}
+    end
+    
+    def report
+      formatted_log_entries.collect do |entry|
+        next unless entry.developer =~ /#{printer.developer}/ || printer.developer.nil?
+        entry.report
+      end.compact
+    end
   end
   
   class LogEntry
     attr_reader :developer, :message, :date, :printer
     def initialize(entry, printer)
       @printer = printer
-      
-      pieces = entry.split(" | ")
-      @developer = pieces[1]
-      @message = pieces.last.split("\n\n").last
-      @date = Date.parse(pieces[2].gsub(/\(.*?\)/, ""))
     end
     
     def report
@@ -42,6 +105,42 @@ class Printlog
     
     def use_developer?
       printer.developer.nil?
+    end
+  end
+  
+  class SVNLogEntry < LogEntry
+    def initialize(entry, printer)
+      super
+      
+      pieces = entry.split(" | ")
+      @developer = pieces[1]
+      @message = pieces.last.split("\n\n").last
+      @date = Date.parse(pieces[2].gsub(/\(.*?\)/, ""))
+    end
+  end
+  
+  class GITLogEntry < LogEntry
+    attr_reader :commit
+    def initialize(entry, printer)
+      super
+      pieces = entry.split("\n")
+      @commit = pieces.shift
+      @developer = parse_developer(pieces)
+      @date = Date.parse(pieces.shift.gsub("Date:   ", ""))
+      @message = parse_message(pieces)
+    end
+    
+    def parse_developer(pieces)
+      while pieces.first !~ /Author/
+        pieces.shift
+      end
+      pieces.shift.gsub("Author: ", "")
+    end
+    
+    def parse_message(full_message)
+      full_message.inject([]) do |output, line|
+        line =~ /^\s{4}.+/ ? (output << line.gsub(/\s{4}/, "")) : output
+      end.join("\n") + "\n"
     end
   end
 end
--- _printlog/printlog_spec.rb
+++ _printlog/printlog_spec.rb
@@ -3,39 +3,40 @@ require "spec"
 require File.dirname(__FILE__) + "/printlog.rb"
 
 describe Printlog do
-  before do
-    @printer = Printlog.new("/Users/siannopollo/Developer/Projects/OpenSource/mail_fetcher")
-  end
-  
-  it "should collect svn log entries and spit them out in a nice format" do
-    @printer.should respond_to(:directory)
-    @printer.should respond_to(:log)
-    @printer.should respond_to(:separator)
-    @printer.should respond_to(:log_entries)
-    @printer.should respond_to(:report)
-  end
-  
-  it "should generate a real report" do
-    @printer.report.should include("* siannopollo [2008-02-15] - setting up directories\n")
-  end
-  
-  describe "log" do
-    before do
-      @printer.stub!(:log).and_return(log)
+  describe "svn" do
+    before(:all) do
+      @printer = Printlog.new("/Users/siannopollo/Developer/Projects/OpenSource/mail_fetcher", :limit => 6)
+      @formatter = @printer.formatter
+      @formatter.stub!(:log).and_return(svn_log)
+    end
+    
+    it "should have some methods" do
+      @printer.should respond_to(:directory)
+      @printer.should respond_to(:limit)
+      @printer.should respond_to(:developer)
+      @printer.should respond_to(:scm)
+      
+      @formatter.should respond_to(:log)
+      @formatter.should respond_to(:log_entries)
+      @formatter.should respond_to(:report)
+    end
+    
+    it "should determine scm type" do
+      @printer.scm.should == "svn"
     end
     
     it "should have a separator extracted from the log" do
-      @printer.separator.should == "------------------------------------------------------------------------"
+      @formatter.separator.should == "------------------------------------------------------------------------"
     end
     
     it "should have entries for the given log" do
-      @printer.log_entries.size.should > 1
-      @printer.log_entries.first.should be_a_kind_of(Printlog::LogEntry)
+      @formatter.formatted_log_entries.size.should == 6
+      @formatter.formatted_log_entries.first.should be_a_kind_of(Printlog::SVNLogEntry)
     end
     
     describe "entries" do
       before do
-        @entry = @printer.log_entries.first
+        @entry = @formatter.formatted_log_entries.first
       end
       
       it "should have a developer, message, and date" do
@@ -62,14 +63,76 @@ describe Printlog do
       end
     end
     
-    describe "format" do
-      it "should sort by dates" do
-        pending "Allow the printer to take a date range and give the logs in that date range"
+  end
+  
+  describe "git" do
+    before(:all) do
+      @printer = Printlog.new("/Users/siannopollo/Developer/Projects/PrepChamps/prepchamps", :limit => 6)
+      @formatter = @printer.formatter
+      @formatter.stub!(:log).and_return(git_log)
+    end
+    
+    it "should determine scm type" do
+      @printer.scm.should == "git"
+    end
+    
+    it "should have a static separator" do
+      @formatter.separator.should == "commit "
+    end
+    
+    it "should have entries for the given log" do
+      @formatter.formatted_log_entries.size.should == 6
+      @formatter.formatted_log_entries.first.should be_a_kind_of(Printlog::GITLogEntry)
+    end
+    
+    describe "entries" do
+      before do
+        @entry = @formatter.formatted_log_entries.first
+      end
+      
+      it "should have a developer, message, and date" do
+        @entry.developer.should == "steve <siannopollo@macBook.local>"
+        @entry.date.to_s.should == "2008-02-28"
+        @entry.message.should == "Merge branch 'master' of git@192.168.50.17:prepchamps\nConflicts:\n	spec/models/user_spec.rb\n"
+      end
+      
+      it "should have a report" do
+        @entry.report.should == "* steve <siannopollo@macBook.local> [2008-02-28] - Merge branch 'master' of git@192.168.50.17:prepchamps\nConflicts:\n	spec/models/user_spec.rb\n"
+      end
+    end
+    
+    describe "report" do
+      it "should generate" do
+        @printer.report.should include("* steve <siannopollo@macBook.local> [2008-02-28] - Merge branch 'master' of git@192.168.50.17:prepchamps\nConflicts:\n	spec/models/user_spec.rb\n")
+        @printer.stub!(:developer).and_return("steve")
+        @printer.report.should include("* [2008-02-28] - Merge branch 'master' of git@192.168.50.17:prepchamps\nConflicts:\n	spec/models/user_spec.rb\n")
+      end
+      
+      it "should be blank if the developer was not found in any of the log entries" do
+        @printer.stub!(:developer).and_return("jown") # Juan Own :-)
+        @printer.report.should be_empty
       end
     end
   end
   
-  def log
+  describe "date sorting" do
+    before(:all) do
+      @printer = Printlog.new("/Users/siannopollo/Developer/Projects/OpenSource/mail_fetcher", :limit => 6, :dates => "2/12/2008-2/14/2008")
+      @formatter = @printer.formatter
+      @formatter.stub!(:log).and_return(svn_log)
+    end
+    
+    it "should have a start date and end date" do
+      @printer.start_date.to_s.should == "2008-02-12"
+      @printer.end_date.to_s.should == "2008-02-14"
+    end
+    
+    it "should only give back entries within a certain date range" do
+      @formatter.formatted_log_entries.size.should == 4
+    end
+  end
+  
+  def svn_log
 %{------------------------------------------------------------------------
 r6 | siannopollo | 2008-02-15 19:37:07 -0500 (Fri, 15 Feb 2008) | 1 line
 
@@ -79,22 +142,67 @@ r5 | siannopollo | 2008-02-15 19:34:44 -0500 (Fri, 15 Feb 2008) | 1 line
 
 finally how i want it
 ------------------------------------------------------------------------
-r4 | siannopollo | 2008-02-15 19:30:50 -0500 (Fri, 15 Feb 2008) | 1 line
+r4 | siannopollo | 2008-02-14 19:30:50 -0500 (Fri, 15 Feb 2008) | 1 line
 
 getting rid of git
 ------------------------------------------------------------------------
-r3 | siannopollo | 2008-02-15 19:28:05 -0500 (Fri, 15 Feb 2008) | 1 line
+r3 | siannopollo | 2008-02-14 19:28:05 -0500 (Fri, 15 Feb 2008) | 1 line
 
 ignoring stuff
 ------------------------------------------------------------------------
-r2 | siannopollo | 2008-02-15 19:24:34 -0500 (Fri, 15 Feb 2008) | 2 lines
+r2 | siannopollo | 2008-02-13 19:24:34 -0500 (Fri, 15 Feb 2008) | 2 lines
 
 Initial import
 
 ------------------------------------------------------------------------
-r1 | siannopollo | 2008-02-15 19:19:34 -0500 (Fri, 15 Feb 2008) | 1 line
+r1 | siannopollo | 2008-02-12 19:19:34 -0500 (Fri, 15 Feb 2008) | 1 line
 
 setting up directories
-------------------------------------------------------------------------}
+------------------------------------------------------------------------
+}
+  end
+  
+  def git_log
+%{commit 37b612ba40f60a73ad2d13784a114dd02fb3c27c
+Merge: 2da5c16... 5dd6d74...
+Author: steve <siannopollo@macBook.local>
+Date:   Thu Feb 28 16:56:02 2008 -0500
+
+    Merge branch 'master' of git@192.168.50.17:prepchamps
+
+    Conflicts:
+
+    	spec/models/user_spec.rb
+
+commit 2da5c16ce79f59abeaa1899c0da1171ac0afee24
+Author: steve <siannopollo@macBook.local>
+Date:   Thu Feb 28 16:54:35 2008 -0500
+
+    Users must activate their account before they can use it
+
+commit 5dd6d7420f0d825d81837c1692d180b57d513f1b
+Author: Matthew Bass <matt@anacreon.local>
+Date:   Thu Feb 28 16:23:01 2008 -0500
+
+    Continued writing order unit tests
+
+commit 98127d143e407b6a842e95a7c665637580b3b68b
+Author: Matthew Bass <matt@anacreon.local>
+Date:   Thu Feb 28 16:09:48 2008 -0500
+
+    Wrote unit tests for Order model
+
+commit 75c574556f1776174460b544538e8585e611c0d0
+Author: Matthew Bass <matt@anacreon.local>
+Date:   Thu Feb 28 15:46:53 2008 -0500
+
+    More test cleanup, added tests to boost coverage
+
+commit 3cdba16633e215a9e0226c7854ff193948dc9fa5
+Merge: aa27f11... 739a12a...
+Author: steve <siannopollo@macBook.local>
+Date:   Thu Feb 28 15:09:05 2008 -0500
+
+    Merge branch 'master' of git@192.168.50.17:prepchamps}
   end
 end
